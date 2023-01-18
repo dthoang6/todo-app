95611585765755b697c528150ae5b01f95806d5f
Your Name <dthoang6@gmail.com>
2023-01-17 19:50:30 -0800

    finish step 4.3 to perform the update oprattion to update item

--- Server.js
+++ Server.js
@@ -2,7 +2,7 @@
 let express = require("express");
 //step 4.1
 //we want to look inside of mongodb package for something called: Mongo Client, and we can use destructuring object to pull out 4 or 5 different things of mongodb package instead of just MongoClient.
-let { MongoClient } = require("mongodb");
+let { MongoClient, ObjectId } = require("mongodb");
 
 /* let mongodb = require(mongodb); //we are not going to use the package directly
  */
@@ -10,6 +10,9 @@ let App = express();
 
 //we need to set up a variable to represents a MongoDB database that we've opened a connection to.
 let db;
+
+App.use(express.static("public")); //we need to tell our Express App or server to allow incoming requests to have access to the public folder and its contents. This is how we can serve up static existing files, it will make the public folder content to be available from the root of our server.
+
 /* //this is the connection string to our database that gives us access or permission.
 //starting by creating a client variable to hold the return of the new instance of Mongo Client.
 let client = new MongoClient("");
@@ -25,7 +28,11 @@ async function go() {
 }
 go();
 
+//tell express to automatically take asynchronous request from browser into our server
+App.use(express.json());
+//tell express to automatically take submitted form data and add it to a body object that lives on the request object.
 App.use(express.urlencoded({ extended: false }));
+
 //step 2: in future we will keep html template in the separated file: hard code fake items.
 App.get("/", function (req, res) {
   /* step 4.2 reading and loading items */
@@ -61,8 +68,8 @@ App.get("/", function (req, res) {
             <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
                 <span class="item-text">${item.text}</span>
                 <div>
-                    <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-                    <button class="delete-me btn btn-danger btn-sm">Delete</button>
+                    <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+                    <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
                 </div>
             </li>
           `;
@@ -71,7 +78,10 @@ App.get("/", function (req, res) {
     </ul>
     
   </div>
-  
+
+  <script src="https://unpkg.com/axios@1.1.2/dist/axios.min.js"></script>
+  <script src="/browser.js"></script>
+
 </body>
 </html>
   `);
@@ -87,5 +97,13 @@ App.post("/create-item", function (req, res) {
   //in this case we use call back function rather modern way, we need to use promise, async/await because we don't know how long it will take to create that item in database.
   db.collection("items").insertOne({ text: req.body.item }, function () {
     res.redirect("/");
+  });
+});
+
+//4.3 receive the data from web browser, communicate with database to update the item.
+App.post("/update-item", function (req, res) {
+  //node will talk to database to update the item
+  db.collection("items").findOneAndUpdate({ _id: new ObjectId(req.body.id) }, { $set: { text: req.body.text } }, function () {
+    res.send("Success.");
   });
 });
---
+++ public/browser.js
@@ -0,0 +1,30 @@
+/* 4.3.tell the web browser to listen for the event when the edit buttons is clicked on */
+
+document.addEventListener("click", function (e) {
+  //if the element that was clicked on contains a class of edit me
+  if (e.target.classList.contains("edit-me")) {
+    //give user a place to type in and save it to a variable.
+    let userInput = prompt("Enter you desired new text:", e.target.parentElement.parentElement.querySelector(".item-text").innerHTML);
+    //we want to send this value in userInput from web browser to our node server. Tell the web browser to send a request to our server without submitting a form or visiting a new url.
+    //we could send a request to the server on the fly or behind the scene using: fetch or Axios, we going to use cdn axios
+    //this is how we can send an on the fly post request to a server.
+    //post(a,b): a = url you want to send a post request to. b = js object, data that's going to get sent along with the request to that. Data that server will receive.
+    //the axios.post(a, b) will return a promise.
+    //we handle the promise using then and catch instead of call back function.
+    //next step is jump to server.js to receive the userInput data from web browser.
+    if (userInput) {
+      //if the user input is not blank, as long as it is exist, the userInput == true.
+      axios
+        .post("/update-item", { text: userInput, id: e.target.getAttribute("data-id") })
+        .then(function () {
+          //4.3.2 this is what will run once when our axios request has had a chance to complete.
+          //we know that our node server is not going to send back its response until its database update action is complete.
+          //manipulate the html interface or manipulate the current item to show the new input text.
+          e.target.parentElement.parentElement.querySelector(".item-text").innerHTML = userInput;
+        })
+        .catch(function () {
+          console.log("Please try again later.");
+        });
+    }
+  }
+});
